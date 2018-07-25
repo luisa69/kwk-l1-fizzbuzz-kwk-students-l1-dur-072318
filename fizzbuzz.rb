7169f8ff8fd87d16fdd5a27d4b95dca22fe7c8be
Luisa Pichardo <luisapichardo@gmail.com>
2018-07-25 18:56:14 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,23 @@
+def fizz_buzz
+x = 18
+ if x % 3 == 0 && x % 5 == 0
+   puts "fizzbuzz"
+  elsif x % 5 == 0
+  puts "buzz" 
+  elsif x % 3 == 0
+  puts "fizz"
+else puts "nothing"
+  end
+end
+
+def bye_felicia
+a = 22
+  if a % 12 == 0 && x % 11 == 0
+    puts "bye felicia"
+    elsif a % 12 == 0
+    puts "bye"
+    elsif a % 11 == 0
+    puts "felicia"
+  else puts "nothing"
+  end
+end 
